96d1c6831a25ebaf4a3f8531a1ba9ba86952ddf3
André Reis <101909552+andrereissz@users.noreply.github.com>
2024-11-13 11:45:33 -0300

    adds initial routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:project_par_fans/telas/home.dart';
+import 'package:project_par_fans/telas/login.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(GetMaterialApp(
+    initialRoute: '/',
+    getPages: [
+      GetPage(name: '/', page: () => Login()),
+      GetPage(name: '/home', page: () => Home()),
+    ],
+    home: Login(),
+  ));
 }
